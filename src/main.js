a96fc623a0375ce28b2bfca8be38a56d880f2d9b
Camilo Avarado <camealvarado5253@gmail.com>
2023-06-28 17:52:20 -0500

    add-galerie-page

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 const principalPage = document.querySelector('.principal-page');
 const simulatorPage = document.querySelector('.simulator-page');
+const galeriePage = document.querySelector('.galerie-page')
 const simulatorPageButton = document.querySelector('.simulator-page-button')
 const principalPageButton = document.querySelector('.principal-page-button')
+const galeriePageButton = document.querySelector('.galerie-page-button')
+const leaveGaleriePageButton = document.querySelector('.leave-galerie-page-button')
 
+
+function principal_page() {
+    principalPage.style.display = 'block'
+    simulatorPage.style.display = 'none'
+    galeriePage.style.display = 'none'
+}
 
 function simulator_page() {
     principalPage.style.display = 'none'
     simulatorPage.style.display = 'block'
 }
 
-function principal_page() {
-    principalPage.style.display = 'block'
-    simulatorPage.style.display = 'none'
+function galerie_page() {
+    principalPage.style.display = 'none'
+    galeriePage.style.display = 'flex'
 }
 
 simulatorPageButton.addEventListener('click', simulator_page);
-principalPageButton.addEventListener('click', principal_page)
+principalPageButton.addEventListener('click', principal_page);
+galeriePageButton.addEventListener('click', galerie_page);
+leaveGaleriePageButton.addEventListener('click', principal_page);
